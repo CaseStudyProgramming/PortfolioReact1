af7746cd5504faf15a2b74aee3cbde1c8409efe0
Daiita30 <davidcristianps675@gmail.com>
2023-07-15 22:46:37 +0700

    menambahkan runner text

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,10 +1,59 @@
 import React from "react";
 import BgGambar1 from "../Assets/unnamed.jpg";
+import { TypeAnimation } from "react-type-animation";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
 
 const Main = () => {
   return (
     <div id="main">
-      <img className="w-full h-screen" src={BgGambar1} alt="" />
+      <img
+        className="w-full h-screen object-cover object-left scale-x-[-1]"
+        src={BgGambar1}
+        alt=""
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800 ">
+            Halo saya David Cristian P.S.
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+            aku
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Developer",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Coder",
+                2000,
+                "Enjoyer",
+                2000,
+                "Gamer",
+                2000,
+              ]}
+              wrapper="div"
+              speed={50}
+              style={{
+                fontSize: "1em",
+                display: "inline-block",
+                paddingLeft: "5px",
+              }}
+              repeat={Infinity}
+            />
+          </h2>
+
+          <div>
+            <FaTwitter />
+            <FaFacebookF />
+            <FaInstagram />
+            <FaLinkedinIn />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
